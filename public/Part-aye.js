0246f3bf5113949d42e08cb307bebdae55e5220f
Matt Green <mgreen9@gmail.com>
2014-03-10 19:41:51 -0700

    added some test data

--- public/Part-aye.js
+++ public/Part-aye.js
@@ -17,7 +17,68 @@ angular.module('PartyApp').controller('PartyCtrl', [
 '$interval',
 function($scope, $http, $interval){
 
-	$scope.messages = [];
+	$scope.messages = [
+	{
+		"ToCountry": "US",
+		"ToState": "VA",
+		"SmsMessageSid": "SM154b23dc63f9ef947c8babaf455b938d",
+		"NumMedia": "0",
+		"ToCity": "VIENNA",
+		"FromZip": "20181",
+		"SmsSid": "SM154b23dc63f9ef947c8babaf455b938d",
+		"FromState": "VA",
+		"SmsStatus": "received",
+		"FromCity": "MANASSAS",
+		"Body": "keep it going allllll night #smileyface",
+		"FromCountry": "US",
+		"To": "+17037634332",
+		"ToZip": "22182",
+		"MessageSid": "SM154b23dc63f9ef947c8babaf455b938d",
+		"AccountSid": "AC50cf5db22487012e70e1eb2e4335bcba",
+		"From": "+17037919734",
+		"ApiVersion": "2010-04-01"
+	},
+	{
+		"ToCountry": "US",
+		"ToState": "VA",
+		"SmsMessageSid": "SM6d91e0d17216bd4506cbe1d5e4fedf81",
+		"NumMedia": "0",
+		"ToCity": "VIENNA",
+		"FromZip": "20181",
+		"SmsSid": "SM6d91e0d17216bd4506cbe1d5e4fedf81",
+		"FromState": "VA",
+		"SmsStatus": "received",
+		"FromCity": "MANASSAS",
+		"Body": "WOOOOOOO",
+		"FromCountry": "US",
+		"To": "+17037634332",
+		"ToZip": "22182",
+		"MessageSid": "SM6d91e0d17216bd4506cbe1d5e4fedf81",
+		"AccountSid": "AC50cf5db22487012e70e1eb2e4335bcba",
+		"From": "+17037919734",
+		"ApiVersion": "2010-04-01"
+	},
+	{
+		"ToCountry": "US",
+		"ToState": "VA",
+		"SmsMessageSid": "SM09752a68e4fdcb1ce0a96215499198cf",
+		"NumMedia": "0",
+		"ToCity": "VIENNA",
+		"FromZip": "20181",
+		"SmsSid": "SM09752a68e4fdcb1ce0a96215499198cf",
+		"FromState": "VA",
+		"SmsStatus": "received",
+		"FromCity": "MANASSAS",
+		"Body": "This is a fun party!",
+		"FromCountry": "US",
+		"To": "+17037634332",
+		"ToZip": "22182",
+		"MessageSid": "SM09752a68e4fdcb1ce0a96215499198cf",
+		"AccountSid": "AC50cf5db22487012e70e1eb2e4335bcba",
+		"From": "+17037919734",
+		"ApiVersion": "2010-04-01"
+	}
+	];
 
 	$interval(function(){
 		$http.get('/retrieve_messages').success(function(data){
