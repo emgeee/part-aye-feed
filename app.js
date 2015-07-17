3e8ff19275d4e079d5ba1aaa0d22ee9df17178dd
Matt Green <mgreen9@gmail.com>
2015-07-16 19:42:07 -0700

    upgrade to express 4.0

--- app.js
+++ app.js
@@ -3,46 +3,35 @@
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes/index.js');
-var http = require('http');
-var path = require('path');
-var sass = require('node-sass');
+var express = require('express')
+var routes = require('./routes/index.js')
+var http = require('http')
+var path = require('path')
+var sass = require('node-sass')
+var bodyParser = require('body-parser')
+var methodOverride = require('method-override')
+var errorHandler = require('errorhandler')
 
-
-
-
-var app = express();
+var app = express()
 
 // all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(
-	sass.middleware({
-		src: __dirname + '/public',
-		dest: __dirname + '/public',
-		debug: true
-	})
-);
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride())
+app.use(express.static(path.join(__dirname, 'public')))
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+if ('development' === app.get('env')) {
+  app.use(errorHandler())
 }
 
-app.get('/', routes.index);
-app.get('/receive_message', routes.receive_message);
-app.get('/retrieve_messages', routes.retrieve_messages);
+app.get('/', routes.index)
+app.get('/receive_message', routes.receive_message)
+app.get('/retrieve_messages', routes.retrieve_messages)
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'))
+})
